cb68e835a5ecc17b38dbd8d75d7e065f920836b4
Tomáš <tomas.reznicek312@gmail.com>
2026-01-18 11:04:05 +0100

    rename

--- backend/log/models.py
+++ backend/log/models.py
@@ -6,7 +6,7 @@ class LogEntry(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     action = models.CharField(max_length=100)
     user = models.CharField(max_length=100) # later can be ForeignKey to User model
-    details = models.TextField()
+    detail = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f"ID: {str(self.uuid)[:8]} - Action: {self.action} - User: {self.user} - Timestamp: {self.timestamp}"
