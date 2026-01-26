caf56fc8829566d0828541d6acf2735d95b2a2b9
Tomáš <119336839+Anzer3@users.noreply.github.com>
2026-01-26 11:26:15 +0100

    api views pro event

--- backend/event/views.py
+++ backend/event/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Event
+from .serializers import EventSerializer
+
+
+@api_view(['GET', 'POST'])
+def event_list(request):
+    if request.method == 'GET':
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            event = serializer.save()
+            return Response(EventSerializer(event).data, status=201)
+        return Response(serializer.errors, status=400)
+    
+@api_view(['GET', 'PATCH', 'DELETE'])
+def event_detail(request, uuid):
+    try:
+        event = Event.objects.get(uuid=uuid)
+    except Event.DoesNotExist:
+        return Response(status=404)
+
+    if request.method == 'GET':
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+
+    elif request.method == 'PATCH':
+        serializer = EventSerializer(event, data=request.data, partial=True)
+        if serializer.is_valid():
+            event = serializer.save()
+            return Response(EventSerializer(event).data)
+        return Response(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        event.delete()
+        return Response(status=204)
