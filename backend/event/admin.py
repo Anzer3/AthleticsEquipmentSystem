c01622b6dc4b3c5be354c37f537866e10e30900d
Tomáš <tomas.reznicek312@gmail.com>
2026-01-29 13:14:31 +0100

    css pro django administraci

--- backend/event/admin.py
+++ backend/event/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Location, EventStatus, Event
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(EventStatus)
+admin.site.register(Event)
